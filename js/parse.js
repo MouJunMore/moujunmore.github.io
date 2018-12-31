e701fe35ee1de786023cac4ff692bb43cbae9b10
MouJunMore <46255349+MouJunMore@users.noreply.github.com>
2018-12-31 22:23:54 +0800

    Update parse.js

--- js/parse.js
+++ js/parse.js
@@ -1,53 +1,38 @@
-//存放导出的数组
-var arrData = new Array();
-var files;
-arrData[0] = '会员名,购买足迹（日销-节日）,购买日期1,类目1,节日1';
-document.getElementById('upfile').onclick = function () {
-    document.getElementById('fileBtn').click();
-}
-String.prototype.trim = function () {
-    return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-}
-
-//判断value 是否在数组里
-var inArray = function (value, arr) {
-    if (typeof (arr) == undefined) return -1;
-    if (typeof (value) == undefined) return -1;
-    for (var i = 0; i < arr.length; i++) {
-        if (value == arr[i]) return i;
+layui.use(['layer', 'form'], function () {
+    var arrData = new Array();
+    var files;
+    arrData[0] = '会员名,购买足迹（日销-节日）,购买日期1,类目1,节日1';
+    document.getElementById('upfile').onclick = function () {
+        document.getElementById('fileBtn').click();
     }
-    return -1;
-}
-
-// 点击解析按钮，开始读取文件
-var upFile = function () {
-    //js 入口程序  模块化加载js
-    layui.use(['layer', 'form'], function () {
-        //读取input上传的文件
+    String.prototype.trim = function () {
+        return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    }
+    var inArray = function (value, arr) {
+        if (typeof (arr) == undefined) return -1;
+        if (typeof (value) == undefined) return -1;
+        for (var i = 0; i < arr.length; i++) {
+            if (value == arr[i]) return i;
+        }
+        return -1;
+    }
+    document.getElementById('parseBtn').onclick = function () {
         files = document.getElementById("fileBtn").files;
-        // 判断有没有选择文件
         if (files.length <= 0) {
             layer.msg('请先选择文件', { icon: 0 });
             return;
         }
-
-        // 
         if (typeof (FileReader) !== 'undefined') {
             var load = layer.load(0);
             var reader = new FileReader();
             reader.readAsText(files[0]);
             reader.onload = function (evt) {
                 var data = evt.target.result;
-                //拿到数据后  分行
                 var rows = data.split("\n");
-                //console.log(rows);
-                // 创建临时数组，保存数据
                 var temp = new Array();
                 for (var i = 1; i < rows.length; i++) {
                     if (rows[i] == '' || typeof (rows[i]) == 'undefined') continue;
-                    //分列
                     var cols = rows[i].split(',');
-                    //console.log(cols    );
                     typeof (cols[3]) == 'undefined' || cols[3].trim() == '' ? cols[3] = '日销' : cols[3] = cols[3];
                     if (typeof (temp[cols[0]]) == 'undefined') {
                         temp[cols[0]] = new Array();
@@ -74,32 +59,23 @@ var upFile = function () {
                 layer.close(load);
                 layer.msg('解析完成！', { icon: 1 });
             }
-
         } else {
-            layer.msg('您使用的浏览器不支持，建议使用Chrome最新版或Firefox浏览器最新版', { icon: 0 });
+            alert("您使用的浏览器不支持，建议使用Chrome最新版或Firefox浏览器最新版");
         }
-    });
-}
-
-// 下载文件
-var loadBtn = function () {
-    layui.use(['layer', 'form'], function () {
+    }
+    document.getElementById('downloadBtn').onclick = function () {
         if (typeof (files) == 'undefined' || files == '' || files.length <= 0) {
             layer.msg('没有解析好的文件可以下载', { icon: 0 });
             return;
         }
-
-        //触发下载
         var str = "\ufeff" + arrData.join("\n");
         console.log(str);
-
-        var blob = new Blob(["\ufeff" + str], { type: 'text/csv' }); //解决大文件下载失败
+        var blob = new Blob(["\ufeff" + str], { type: 'text/csv' }); 
         var downloadLink = document.createElement("a");
         downloadLink.setAttribute("href", URL.createObjectURL(blob));
-        downloadLink.setAttribute("download", "客户信息表.csv");
+        downloadLink.setAttribute("download", "my_data.csv");
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
-    })
-
-}
+    }
+});
